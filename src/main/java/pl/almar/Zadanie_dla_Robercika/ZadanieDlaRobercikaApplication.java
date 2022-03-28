d81dd50f4d5eb2a2f7409c1537a6b44eac8d9553
adamskyyy <adamskialeksander@gmail.com>
2022-03-28 12:21:06 +0200

    a

---
+++ src/main/java/pl/almar/Zadanie_dla_Robercika/ZadanieDlaRobercikaApplication.java
@@ -0,0 +1,13 @@
+package pl.almar.Zadanie_dla_Robercika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieDlaRobercikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZadanieDlaRobercikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/almar/Zadanie_dla_Robercika/ZadanieDlaRobercikaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.almar.Zadanie_dla_Robercika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadanieDlaRobercikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
